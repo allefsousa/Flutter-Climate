c2e763289028ad47aae99e7487d78f86e61421b4
Allef Sousa <allefsousa_1@hotmail.com>
2020-03-02 22:28:16 -0300

    Finalizando a tela de buscas

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,23 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Climate App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,56 +26,184 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: SearchPage()
-    );
+        resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomPadding: false,
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: SearchPage());
   }
-
 }
+
 class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bottom = MediaQuery.of(context).viewInsets.bottom;
 
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Center(
-
-        ),
-        Container(
-
-        )
-      ],
+    return SingleChildScrollView(
+      reverse: true,
+      padding: EdgeInsets.only(bottom: bottom),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Container(
+              child: FlareActor(
+                "assets/flare/WorldSpin.flr",
+                fit: BoxFit.contain,
+                animation: "roll",
+              ),
+              height: 300,
+              width: 300,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              left: 32,
+              right: 32,
+            ),
+            child: Column(
+              children: <Widget>[
+                Text("Search weather",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white70)),
+                Text(
+                  "Instanly",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.white70),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.search,
+                        color: Colors.white70,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          borderSide: BorderSide(
+                              color: Colors.white70, style: BorderStyle.solid)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          borderSide: BorderSide(
+                              color: Colors.blue, style: BorderStyle.solid)),
+                      hintText: "City Name",
+                      hintStyle: TextStyle(color: Colors.white70)),
+                  keyboardType: TextInputType.text,
+                  textInputAction: TextInputAction.go,
+                  style: TextStyle(
+                    color: Colors.white70,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 50,
+                  child: FlatButton(
+                    shape: new RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(6))),
+                    onPressed: () {},
+                    color: Colors.lightBlue,
+                    child: Text(
+                      "Search",
+                      style: TextStyle(color: Colors.white70, fontSize: 16),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
+class ShowWeather extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.only(right: 32, left: 32, top: 10),
+        child: Column(
+          children: <Widget>[
+            Text(
+              "city",
+              style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "".toString() + "C",
+              style: TextStyle(color: Colors.white70, fontSize: 50),
+            ),
+            Text(
+              "Temprature",
+              style: TextStyle(color: Colors.white70, fontSize: 14),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Min Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "C",
+                      style: TextStyle(color: Colors.white70, fontSize: 30),
+                    ),
+                    Text(
+                      "Max Temprature",
+                      style: TextStyle(color: Colors.white70, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: FlatButton(
+                shape: new RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                onPressed: () {},
+                color: Colors.lightBlue,
+                child: Text(
+                  "Search",
+                  style: TextStyle(color: Colors.white70, fontSize: 16),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
